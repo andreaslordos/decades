1eb44c42323132ee4e121d8e9d4e58b1afa4856f
Andreas Lordos <andreasglordos@gmail.com>
2024-04-22 14:27:40 -0400

    added tomorrows

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,48 +1,107 @@
 export const CONNECTION_GAMES = [
-  // [
-  //   {
-  //     category: "1960s",
-  //     words: ["KENNEDY IS KILLED BY SNIPER AS HE RIDES IN CAR...", "MEN WALK ON MOON", "FURTHER UPHEAVAL IN CHINA FORESEEN IN POWER BATTLE...", "EAST GERMAN TROOPS SEAL BORDER WITH WEST BERLIN TO BLOCK REFUGEE ESCAPE"],
-  //     difficulty: 2,
-  //   },
-  //   {
-  //     category: "2000s",
-  //     words: ["BUSH APPEARS TO DEFEAT GORE", "IN SWEEPING MOVE, FED BACKS BUYOUT AND WALL ST. LOANS", "2 ENRON CHIEFS ARE CONVICTED IN FRAUD AND CONSPIRACY TRIAL", "BUSH DECLARES 'ONE VICTORY IN A WAR ON TERROR'"],
-  //     difficulty: 1,
-  //   },
-  //   {
-  //     category: "1930s",
-  //     words: ["NINTH PLANET DISCOVERED ON EDGE OF SOLAR SYSTEM; ...", "GANDHI SEIZED BY BRITISH FOR INDEFINITE...", "BOTH HOUSES OVERWHELM PRESIDENT'S VETO OF BILL ON SPANISH WAR PENSIONS", "U.S. CONDEMNS JAPAN AS INVADER OF CHINA; DROPS NEUTRALITY POLICY..."],
-  //     difficulty: 3,
-  //   },
-  //   {
-  //     category: "1910s",
-  //     words: ["TITANIC SINKS FOUR HOURS AFTER HITTING ICEBERG", "HEIR TO AUSTRIA'S THRONE IS SLAIN WITH HIS WIFE BY A BOSNIAN YOUTH...", "EDWARD VII. DIES OF PNEUMONIA; GEORGE V. REIGNS OVER ENGLAND", "TWO HELD FOR BIG STOCK STAMP STEAL"],
-  //     difficulty: 4,
-  //   },
-  // ],
-  // [
-  //   {
-  //     category: "1980s",
-  //     words: ["THE SHUTTLE EXPLODES - 6 IN CREW AND HIGH SCHOOL...", "REAGAN EASILY BEATS CARTER; REPUBLICANS GAIN IN CONGRESS", "SOVIET, REPORTING ATOM PLANT 'DISASTER,' SEEKS HELP..", "JOHN LENNON OF BEATLES IS KILLED; SUSPECT HELD..."],
-  //     difficulty: 3,
-  //   },
-  //   {
-  //     category: "1990s",
-  //     words: ["DIANA KILLED IN A CAR ACCIDENT IN PARIS", "GORBACHEV, LAST SOVIET LEADER, RESIGNS", "CLINTON ADMITS LEWINSKY LIAISON TO JURY", "THATCHER SAYS SHE'LL QUIT; 11 1/2 YEARS AS PRIME..."],
-  //     difficulty: 2,
-  //   },
-  //   {
-  //     category: "1940s",
-  //     words: ["JAPAN WARS ON U.S. AND BRITAIN; MAKES SUDDEN ATTACK...", "THE WAR IN EUROPE IS ENDED! SURRENDER ...", "TROTSKY DIES OF HIS WOUNDS; ASKS REVOLUTION ....", "INDIA AND PAKISTAN BECOME NATIONS; CLASHES CONTINUE"],
-  //     difficulty: 4,
-  //   },
-  //   {
-  //     category: "2020s",
-  //     words: ["U.S. DEATHS NEAR 100,000, AN INCALCULABLE LOSS", "TRUMP INCITES MOB", "ROE OVERTURNED", "TALIBAN CAPTURE KABUL, STUNNING U.S. AS A 20-YEAR EFFORT.."],
-  //     difficulty: 1,
-  //   },
-  // ]
+  [
+    {
+      category: "1960s",
+      words: ["KENNEDY IS KILLED BY SNIPER AS HE RIDES IN CAR...", 
+              "MEN WALK ON MOON", 
+              "FURTHER UPHEAVAL IN CHINA FORESEEN IN POWER BATTLE...", 
+              "EAST GERMAN TROOPS SEAL BORDER WITH WEST BERLIN TO BLOCK REFUGEE ESCAPE"],
+      difficulty: 2,
+    },
+    {
+      category: "2000s",
+      words: ["BUSH APPEARS TO DEFEAT GORE", 
+              "IN SWEEPING MOVE, FED BACKS BUYOUT AND WALL ST. LOANS", 
+              "2 ENRON CHIEFS ARE CONVICTED IN FRAUD AND CONSPIRACY TRIAL", 
+              "BUSH DECLARES 'ONE VICTORY IN A WAR ON TERROR'"],
+      difficulty: 1,
+    },
+    {
+      category: "1930s",
+      words: ["NINTH PLANET DISCOVERED ON EDGE OF SOLAR SYSTEM; ...", 
+              "GANDHI SEIZED BY BRITISH FOR INDEFINITE...", 
+              "BOTH HOUSES OVERWHELM PRESIDENT'S VETO OF BILL ON SPANISH WAR PENSIONS", 
+              "U.S. CONDEMNS JAPAN AS INVADER OF CHINA; DROPS NEUTRALITY POLICY..."],
+      difficulty: 3,
+    },
+    {
+      category: "1910s",
+      words: ["TITANIC SINKS FOUR HOURS AFTER HITTING ICEBERG", 
+              "HEIR TO AUSTRIA'S THRONE IS SLAIN WITH HIS WIFE BY A BOSNIAN YOUTH...", 
+              "EDWARD VII. DIES OF PNEUMONIA; GEORGE V. REIGNS OVER ENGLAND", 
+              "TWO HELD FOR BIG STOCK STAMP STEAL"],
+      difficulty: 4,
+    },
+  ],
+  [
+    {
+      category: "1980s",
+      words: ["THE SHUTTLE EXPLODES - 6 IN CREW AND HIGH SCHOOL...", 
+              "REAGAN EASILY BEATS CARTER; REPUBLICANS GAIN IN CONGRESS", 
+              "SOVIET, REPORTING ATOM PLANT 'DISASTER,' SEEKS HELP..", 
+              "JOHN LENNON OF BEATLES IS KILLED; SUSPECT HELD..."],
+      difficulty: 3,
+    },
+    {
+      category: "1990s",
+      words: ["DIANA KILLED IN A CAR ACCIDENT IN PARIS", 
+              "GORBACHEV, LAST SOVIET LEADER, RESIGNS", 
+              "CLINTON ADMITS LEWINSKY LIAISON TO JURY", 
+              "THATCHER SAYS SHE'LL QUIT; 11 1/2 YEARS AS PRIME..."],
+      difficulty: 2,
+    },
+    {
+      category: "1940s",
+      words: ["JAPAN WARS ON U.S. AND BRITAIN; MAKES SUDDEN ATTACK...", 
+              "THE WAR IN EUROPE IS ENDED! SURRENDER ...", 
+              "TROTSKY DIES OF HIS WOUNDS; ASKS REVOLUTION ....", 
+              "INDIA AND PAKISTAN BECOME NATIONS; CLASHES CONTINUE"],
+      difficulty: 4,
+    },
+    {
+      category: "2020s",
+      words: ["U.S. DEATHS NEAR 100,000, AN INCALCULABLE LOSS", 
+              "TRUMP INCITES MOB", 
+              "ROE OVERTURNED", 
+              "TALIBAN CAPTURE KABUL, STUNNING U.S. AS A 20-YEAR EFFORT.."],
+      difficulty: 1,
+    },
+  ],
+  [
+    {
+      category: "2010s",
+      words: ["QADDAFI, SEIZED BY FOES, MEETS A VIOLENT END", 
+              "POPE RESIGNS, WITH CHURCH AT CROSSROADS", 
+              "JETLINER EXPLODES OVER UKRAINE; STRUCK BY A MISSILE...", 
+              "POWERFUL QUAKE AND TSUNAMI DEVESTATE NORTHERN JAPAN"],
+      difficulty: 1,
+    },
+    {
+      category: "1970s",
+      words: ["WIDE FIGHTING RAGES IN CYPRUS, TURKS CONTROL ROAD TO NICOSIA..", 
+              "NIXON RESIGNS",
+              "CAMBODIA REDS ARE UPROOTING MILLIONS AS THEY IMPOSE ...", 
+              "THREE MILE ISLAND ACCIDENT: A CLOUD OVER ATOM POWER"
+            ],
+      difficulty: 2,
+    },
+    {
+      category: "1950s",
+      words: ["CHURCHILL QUITS AS PRIME MINISTER; DECLINES DUKEDOM TO STAY AN M.P...", 
+              "HOUSE ACTS TO GIVE ASIANS CITIZENSHIP AND END RACE BARS", 
+              "ALASKA TO JOIN UNION AS 49TH STATE; FINAL APPROVAL IS VOTED BY SENATE...", 
+              "HIGH COURT BANS SCHOOL SEGREGATION; 9-TO-0 DECISION GRANTS TIME TO COMPLY"],
+      difficulty: 3,
+    },
+    {
+      category: "1900s",
+      words: ["CZAR'S SUBJECTS ARM FOR REVOLT",
+              "WRIGHT AEROPLANE GOES THREE MILES",
+              "NEW YORK TO LONDON IN 5 DAYS 8 HOURS",
+              "ILLINOIS MOBS KILL AND BURN"],
+      difficulty: 4,
+    }
+  ],
   [
     {
       category: "2010s",
@@ -76,6 +135,43 @@ export const CONNECTION_GAMES = [
               "COMMUNISTS PLOT TO USE ROOSEVELT IN AIMS, FREY SAYS",
               "REICH TO SEEK LARGE LOAN, PARLEY IN LONDON JULY 17…"],
       difficulty: 4,
+    }
+  ],
+  [
+    {
+      category: "2000s",
+      words: ["JACKSON CLEARED AFTER 14-WEEK CHILD MOLESTING TRIAL",
+              "FOR FIRST TIME, NUCLEAR FAMILIES DROP BELOW 25% OF HOUSEHOLDS",
+              "BUSH WAS WARNED BIN LADEN WANTED TO HIJACK PLANES",
+              "PUTIN IS MADE RUSSIA'S FIRST PRESIDENT IN FIRST FREE TRANSFER OF POWER"],
+      difficulty: 1
+    },
+    {
+      category: "1960s",
+      words: ["U.S. Imposes Arms Blockade on Cuba on Finding Offensive Missile Sites..",
+              "Marilyn Monroe Dead, Pills Near",
+              "Martin Luther King is slain in memphis; a white is suspected...",
+              "israeli and arab forces battling; both claim land and air victories.."
+            ],
+      difficulty: 2
+    },
+    {
+      category: "1940s",
+      words: ["NEW TOKYO CHIEF PROMISES TO BACK M'ARTHUR IN FULL",
+              "Zionists proclaim new state of israel; truman recognizes it and hopes for peace...",
+              "Atomic Blast, on Schedule, May Rip Hole in Sea Floor",
+              "Roosevelt wins fourth term; record popular vote is close; democrats gain.."
+          ],
+      difficulty: 3
+    },
+    {
+      category: "1920s",
+      words: ["Stocks collapse in 16,410,030-share day, but rally at close cheers brokers...",
+              "TENNESSEE COMPLETES SUFFRAGE VICTORY",
+              "MAY MAKE REFUND TO LIQUOR OWNERS",
+              "150,000 ROMANS HAIL MUSSOLINI'S RETURN BY JOYOUS OVATION"
+            ],
+      difficulty: 4
     }
   ]
 ];
